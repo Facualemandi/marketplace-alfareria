a92b9f8f4590ea41202fc27abface751070d1c3d
Facundo Alemandi <facuinstituto10@gmail.com>
2022-05-27 21:45:36 -0300

    Modificando el menu alteral

--- src/Components/OpenMenuLateral/OpenMenuLateral.js
+++ src/Components/OpenMenuLateral/OpenMenuLateral.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './OpenMenuLateral.css'
+import { BiHomeAlt } from "react-icons/bi";
+import { RiContactsLine } from "react-icons/ri";
+import { FcInfo } from "react-icons/fc";
+import { IoMdContacts } from "react-icons/io";
 
 const OpenMenuLateral = ({openMenu}) => {
   return (
       <> 
-         {openMenu && <section className='scontainer_Menu_lateral'>
+         {openMenu && <section className={`scontainer_Menu_lateral ${openMenu && 'is-active'}`}>
            <ul className='ul_menu_lateral'>
-             <li>Facundo </li>
-             <li>Alemandi</li>
-             <li>Alemandi</li>
-             <li>Alemandi</li>
+             <li> <BiHomeAlt className='icon_menu_lateral'/> Home </li>
+             <li> <RiContactsLine className='icon_menu_lateral'/> Contacto</li>
+             <li> <IoMdContacts className='icon_menu_lateral'/> Sobe nosotros</li>
+             <li> <FcInfo className='icon_menu_lateral'/> Mas información</li>
            </ul>
          </section>}
         
