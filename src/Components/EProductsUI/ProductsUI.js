cda13a618603348c18ce7ccf039838ef24539027
Facundo Alemandi <facuinstituto10@gmail.com>
2022-05-30 23:27:16 -0300

    Quitando el ver más de las fotos

--- src/Components/EProductsUI/ProductsUI.js
+++ src/Components/EProductsUI/ProductsUI.js
@@ -6,24 +6,23 @@ import { BsArrowRightShort } from "react-icons/bs";
 const ProductsUI = ({ image, name, price, el, seeProduct, size }) => {
   return (
     <>
-      <section className="container_prodcts_ui">
-        <section className="section_products_ui">
-          <img src={image} alt={name} className="img_producs_ui" />
-        </section>
+      <NavLink
+        to={`/description/${name}`}
+        className="decoration"
+        onClick={() => seeProduct(el)}
+      >
+        <section className="container_prodcts_ui">
+          <section className="section_products_ui">
+            <img src={image} alt={name} className="img_producs_ui" />
+          </section>
 
-        <section className="section_description_product">
-          <p>{name}</p>
-          <p>{size} </p>
-          <span>${price}</span>
+          <section className="section_description_product">
+            <p>{name}</p>
+            <p>{size} </p>
+            <span>${price}</span>
+          </section>
         </section>
-
-        <NavLink to={`/description/${name}`} className="decoration">
-          <p className="more_product" onClick={() => seeProduct(el)}>
-            {" "}
-            Ver más <BsArrowRightShort />{" "}
-          </p>
-        </NavLink>
-      </section>
+      </NavLink>
     </>
   );
 };
