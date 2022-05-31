4d1287f2dc5b7af37774a84f2a41eb131aba27a3
Facundo Alemandi <facuinstituto10@gmail.com>
2022-05-31 14:27:06 -0300

    validando el nombre y el apellido

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,7 +1,8 @@
 import "./Data.css";
 import React, { useState } from "react";
 import DataEnvio from "../DataEnvío/DataEnvio";
-import { RiWhatsappFill } from 'react-icons/ri';
+import { RiWhatsappFill } from "react-icons/ri";
+import { AiFillWarning } from "react-icons/ai";
 
 const initialValue = {
   name: "",
@@ -12,6 +13,11 @@ const Data = ({ productInTheCart }) => {
   const [retiro, setRetiro] = useState(false);
   const [envio, setEnvio] = useState(false);
   const [form, setForm] = useState(initialValue);
+
+  const [errorName, setErrorName] = useState(false);
+  const [errorSurname, setErrorSurname] = useState(false);
+  const [delivery, setDelivery] = useState(false);
+  const [payment, setPayment] = useState(false);
 
   const retiroProduct = () => {
     if (!retiro) {
@@ -37,6 +43,20 @@ const Data = ({ productInTheCart }) => {
     });
   };
 
+  const valueErrors = (e) => {
+    e.preventDefault();
+    if (!form.name) {
+      setErrorName(true);
+    }else{
+      setErrorName(false)
+    }
+    if(!form.apellido){
+      setErrorSurname(true)
+    }else{
+      setErrorSurname(false)
+    }
+  };
+
   const products = productInTheCart.map((el) => {
     const isProduct = el.name;
     const isAmount = el.amount;
@@ -52,12 +72,18 @@ const Data = ({ productInTheCart }) => {
           <div className="container_name_surname">
             <label for="name">Nombre:</label>
             <input
+              className={`${errorName && "input_error_name"}`}
               type={"text"}
               name="name"
               placeholder="Enter your name"
               onChange={changeValue}
               value={form.name}
             />
+            {errorName && (
+              <p className="error_name">
+                Por favor, ingresa un nombre <AiFillWarning />
+              </p>
+            )}
           </div>
 
           <div className="container_name_surname">
@@ -68,7 +94,13 @@ const Data = ({ productInTheCart }) => {
               placeholder="Enter your surname"
               onChange={changeValue}
               value={form.apellido}
+              className={`${errorSurname && "input_error_name"}`}
             />
+            {errorSurname && (
+              <p className="error_name">
+                Por favor, ingresa un apellido <AiFillWarning />
+              </p>
+            )}
           </div>
         </form>
 
@@ -78,6 +110,11 @@ const Data = ({ productInTheCart }) => {
             <p onClick={retiroProduct}>Lo retiro personalmente</p>
             <p onClick={envioProduct}>Necesito que me lo envíen</p>
           </section>
+          {delivery && (
+            <p className="delivery">
+              La forma de entrega es obligatoria <AiFillWarning />
+            </p>
+          )}
 
           {envio && <DataEnvio />}
         </section>
@@ -96,15 +133,21 @@ const Data = ({ productInTheCart }) => {
             <p>Efectivo</p>
             <p>Transferencia</p>
           </section>
+          {delivery && (
+            <p className="delivery">
+              La forma de entrega es obligatoria <AiFillWarning />
+            </p>
+          )}
         </section>
 
         <section className="section_link_wsp">
           <a
+            onClick={valueErrors}
             className="link_whatapp"
             href={`https://api.whatsapp.com/send?phone=543517653448&text=Hola!%20Me%20llamo%20${form.name}%20${form.apellido}%20Y%20quiero%20pedirte%20${products[0]}%20${products[1]}`}
           >
             Hacer pedido por WhatsApp
-            <RiWhatsappFill className="icon_wsp"/>
+            <RiWhatsappFill className="icon_wsp" />
           </a>
         </section>
       </section>
