4e89e6ab98b56a1d443e46de16ccb0585138a4cc
Facundo Alemandi <facuinstituto10@gmail.com>
2022-06-02 14:44:46 -0300

    dasd

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -21,58 +21,81 @@ const Data = ({ productInTheCart }) => {
     return [isAmount, isProduct];
   });
 
-  let mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+  let mobile = [
+    "iphone",
+    "ipad",
+    "android",
+    "blackberry",
+    "nokia",
+    "opera mini",
+    "windows mobile",
+    "windows phone",
+    "iemobile",
+  ];
 
-  function isMobile(){
+  function isMobile() {
     let agent = navigator.userAgent;
-    mobile.map(el => {
-       if(agent.toLowerCase().indexOf(el.toLowerCase()) > 0 ){
-         return true
-       }else{
-         return false
-       }
-    })
-
+    mobile.map((el) => {
+      if (agent.toLowerCase().indexOf(el.toLowerCase()) > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    });
   }
 
-  const urlDesktop = 'https://web.whatsapp.com/';
-  const urlMobile = 'whatsapp://';
-  const telefono = '543517653448';
+  const urlDesktop = "https://web.whatsapp.com/";
+  const urlMobile = "whatsapp://";
+  const telefono = "543517653448";
 
   const onSubmit = (e) => {
-    e.preventDefault()
-  
-        let mensaje = `send?phone= + ${telefono} + &text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A + ${form.nombre} %0A*¿Cuáles son tus apellidos?*%0A + ${form.apellido}`
-         if(isMobile()){
-              window.open(urlMobile + mensaje, '_blank')
-         }else{
-          window.open(urlDesktop + mensaje, '_blank')
-         }
+    e.preventDefault();
 
-  }
+    let mensaje = `send?phone= + ${telefono} + &text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A + ${form.nombre} %0A*¿Cuáles son tus apellidos?*%0A + ${form.apellido}`;
+    if (isMobile()) {
+      window.open(urlMobile + mensaje, "_blank");
+    } else {
+      window.open(urlDesktop + mensaje, "_blank");
+    }
+  };
 
   const onChangeValue = (e) => {
-      setForm({
-        ...form,
-        [e.target.name]:e.target.value
-      })
-  }
-
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   console.log(products);
   return (
-    <form id="formulario" class="formulario"  onSubmit={onSubmit}>
-      <h1>Último paso</h1>
-      <div class="box-input">
-        <input name="nombre"  type="text" value={form.name} onChange={onChangeValue} required />
-        <label>Nombre</label>
-      </div>
-      <div class="box-input">
-        <input name="apellido" type="text" value={form.apellido} onChange={onChangeValue} required />
-        <label>Apellido</label>
-      </div>
-      <button id="submit" type="submit" class="boton"><i class="fab fa-whatsapp"></i> Enviar WhatsApp</button>
-    </form>
+    <>
+      <form id="formulario" class="formulario" onSubmit={onSubmit}>
+        <h1>Último paso</h1>
+        <div class="box-input">
+          <input
+            name="nombre"
+            type="text"
+            value={form.name}
+            onChange={onChangeValue}
+            required
+          />
+          <label>Nombre</label>
+        </div>
+        <div class="box-input">
+          <input
+            name="apellido"
+            type="text"
+            value={form.apellido}
+            onChange={onChangeValue}
+            required
+          />
+          <label>Apellido</label>
+        </div>
+        <button id="submit" type="submit" class="boton">
+          <i class="fab fa-whatsapp"></i> Enviar WhatsApp
+        </button>
+      </form>
+    </>
   );
 };
 
